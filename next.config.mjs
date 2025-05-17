b8fba5446eb6653be105404b8f591419917e44e7
aborgas90 <wildanmuh7@gmail.com>
2025-05-17 07:56:41 +0700

    fix:  setting next

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // Fallback values jika diperlukan
     NEXT_PUBLIC_FILE_BASE_URL: process.env.NEXT_PUBLIC_FILE_BASE_URL,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${process.env.NEXT_PUBLIC_APP_URL}/:path*`,
+      },
+    ];
   },
 };
 
